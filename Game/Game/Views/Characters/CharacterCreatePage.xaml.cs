487ebc7a7ba7a73737469bca623e7af9beb6e6bc
Yu Fu <yf23@outlook.com>
2021-01-30 19:50:20 -0800

    Remove unused layouts and methods in CharacterCreagePage. Changed CharacterCreatePage.xaml and CharacterCreatePage.xaml.cs.

--- Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
@@ -14,18 +14,18 @@ namespace Game.Views
     /// </summary>
     [DesignTimeVisible(false)] 
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ItemCreatePage : ContentPage
+    public partial class CharacterCreatePage : ContentPage
     {
         // The item to create
         public GenericViewModel<ItemModel> ViewModel = new GenericViewModel<ItemModel>();
 
         // Empty Constructor for UTs
-        public ItemCreatePage(bool UnitTest){}
+        public CharacterCreatePage(bool UnitTest){}
 
         /// <summary>
         /// Constructor for Create makes a new model
         /// </summary>
-        public ItemCreatePage()
+        public CharacterCreatePage()
         {
             InitializeComponent();
 
@@ -35,66 +35,6 @@ namespace Game.Views
 
             this.ViewModel.Title = "Create";
 
-            //Need to make the SelectedItem a string, so it can select the correct item.
-            LocationPicker.SelectedItem = ViewModel.Data.Location.ToString();
-            AttributePicker.SelectedItem = ViewModel.Data.Attribute.ToString();
-        }
-
-        /// <summary>
-        /// Save by calling for Create
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public async void Save_Clicked(object sender, EventArgs e)
-        {
-            // If the image in the data box is empty, use the default one..
-            if (string.IsNullOrEmpty(ViewModel.Data.ImageURI))
-            {
-                ViewModel.Data.ImageURI = Services.ItemService.DefaultImageURI;
-            }
-
-            MessagingCenter.Send(this, "Create", ViewModel.Data);
-            await Navigation.PopModalAsync();
-        }
-
-        /// <summary>
-        /// Cancel the Create
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public async void Cancel_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PopModalAsync();
-        }
-
-        /// <summary>
-        /// Catch the change to the Stepper for Range
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void Range_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            RangeValue.Text = String.Format("{0}", e.NewValue);
-        }
-
-        /// <summary>
-        /// Catch the change to the stepper for Value
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void Value_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            ValueValue.Text = String.Format("{0}", e.NewValue);
-        }
-
-        /// <summary>
-        /// Catch the change to the stepper for Damage
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void Damage_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            DamageValue.Text = String.Format("{0}", e.NewValue);
         }
     }
 }
