0646a26465608041dfcab3502c651c6aac9bf263
Yu Fu <yf23@outlook.com>
2021-02-03 11:50:16 -0800

    Add Level Picker to CharacterUpdatePage. Change CharacterUpdagePage.xaml and CharacterUpdatePage.xaml.cs

--- Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
@@ -5,6 +5,8 @@ using Xamarin.Forms.Xaml;
 
 using Game.ViewModels;
 using Game.Models;
+using Game.GameRules;
+
 
 namespace Game.Views
 {
@@ -32,9 +34,7 @@ namespace Game.Views
 
             this.ViewModel.Title = "Character Update";
 
-            ////Need to make the SelectedItem a string, so it can select the correct item.
-            //LocationPicker.SelectedItem = data.Data.Location.ToString();
-            //AttributePicker.SelectedItem = data.Data.Attribute.ToString();
+            LoadLevelPickerValues();
         }
 
         /// <summary>
@@ -63,6 +63,49 @@ namespace Game.Views
         {
             await Navigation.PopModalAsync();
         }
+
+        #region Picker
+        /// <summary>
+        /// Load the values for the Level Picker
+        /// </summary>
+        /// <returns></returns>
+        public bool LoadLevelPickerValues()
+        {
+            // Load the values for the Level into the Picker
+            for (var i = 1; i <= LevelTableHelper.MaxLevel; i++)
+            {
+                LevelPicker.Items.Add(i.ToString());
+            }
+
+            LevelPicker.SelectedIndex = -1;
+
+            return true;
+        }
+
+        /// <summary>
+        /// The Level selected from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void LevelPicker_Changed(object sender, EventArgs args)
+        {
+            // If the Picker is not set, then set it
+            if (LevelPicker.SelectedIndex == -1)
+            {
+                LevelPicker.SelectedIndex = ViewModel.Data.Level - 1;
+                return;
+            }
+
+            var result = LevelPicker.SelectedIndex + 1;
+
+            // Only roll again for health if the level changed.
+            if (result != ViewModel.Data.Level)
+            {
+                // Change the Level
+                ViewModel.Data.Level = result;
+            }
+        }
+        #endregion Picker
 
     }
 }
