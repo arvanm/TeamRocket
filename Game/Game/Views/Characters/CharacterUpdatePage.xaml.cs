e9b3c9828fdd18a392ce2930e1c8cd4bee6e710e
Yu Fu <yf23@outlook.com>
2021-02-05 02:13:12 -0800

    Display alert at save if the current health field is less than 0 or larger than max health in CharacterUpdatePage.xaml.cs

--- Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
@@ -111,6 +111,24 @@ namespace Game.Views
 
             return true;
         }
+
+        /// <summary>
+        /// Check whether the input current health is in the correct range.
+        /// If the input current health is smaller than 0 or larger than the max health,
+        /// display an alert, and return false.
+        /// Otherwise return true
+        /// </summary>
+        /// <returns>Whether the input name is empty or null</returns>
+        private async Task<bool> CheckCharacterCurHealth()
+        {
+            if (ViewModel.Data.CurrentHealth < 0 || ViewModel.Data.CurrentHealth > ViewModel.Data.MaxHealth)
+            {
+                await DisplayAlert("Alert", "Character current health must between 0 and Max Health!", "OK");
+                return false;
+            }
+
+            return true;
+        }
         #endregion InputValueCheck
 
         /// <summary>
@@ -124,7 +142,7 @@ namespace Game.Views
             // the character name is not empty,
             // the max health is not within [1, level * 10] range,
             // otherwise display an alert.
-            if (await CheckCharacterName() && await CheckCharacterMaxHealth())
+            if (await CheckCharacterName() && await CheckCharacterMaxHealth() && await CheckCharacterCurHealth())
             {
                 // If the image in the data box is empty, use the default one..
                 if (string.IsNullOrEmpty(ViewModel.Data.ImageURI))
