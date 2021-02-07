050744b59b45d0e0609d2bd6f434c5354b94fe4b
Yu Fu <yf23@outlook.com>
2021-02-06 21:37:27 -0800

    Add MonsterReadPage.xaml and MonsterReadPage.xaml.cs in Views\Monsters

---
+++ Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Game.ViewModels;
+using Game.Models;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// The Read Page
+    /// </summary>
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MonsterReadPage : ContentPage
+    {
+        // View Model for Item
+        public readonly GenericViewModel<MonsterModel> ViewModel;
+
+        // Empty Constructor for UTs
+        public MonsterReadPage(bool UnitTest) { }
+
+        /// <summary>
+        /// Constructor called with a view model
+        /// This is the primary way to open the page
+        /// The viewModel is the data that should be displayed
+        /// </summary>
+        /// <param name="viewModel"></param>
+        public MonsterReadPage(GenericViewModel<MonsterModel> data)
+        {
+            InitializeComponent();
+
+            BindingContext = this.ViewModel = data;
+            
+            this.ViewModel.Title = "Monster Read";
+
+        }
+
+        /// <summary>
+        /// Save calls to Update
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Update_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new MonsterUpdatePage(ViewModel)));
+            await Navigation.PopAsync();
+        }
+
+        /// <summary>
+        /// Calls for Delete
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Delete_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new MonsterDeletePage(ViewModel)));
+            await Navigation.PopAsync();
+        }
+    }
+}
