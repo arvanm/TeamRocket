093e528d1cab4ac6c9ffb952373a64be346206b3
Yu Fu <yf23@outlook.com>
2021-02-06 21:35:08 -0800

    Add MonsterIndexViewModel.cs in ViewModel folder.

---
+++ Game/Game/ViewModels/MonsterIndexViewModel.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+using Game.Models;
+using Game.Views;
+using Game.GameRules;
+
+namespace Game.ViewModels
+{
+    /// <summary>
+    /// Monster View Model
+    /// Manages the list of data records
+    /// </summary>
+    public class MonsterIndexViewModel : BaseViewModel<MonsterModel>
+    {
+        #region Singleton
+
+        // Make this a singleton so it only exist one time because holds all the data records in memory
+        private static volatile CharacterIndexViewModel instance;
+        private static readonly object syncRoot = new Object();
+
+        public static CharacterIndexViewModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new CharacterIndexViewModel();
+                            instance.Initialize();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion Singleton
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// 
+        /// The constructor subscribes message listeners for crudi operations
+        /// </summary>
+        public MonsterIndexViewModel()
+        {
+            Title = "Monsters";
+
+            #region Messages
+
+            // Register the Create Message
+            MessagingCenter.Subscribe<CharacterCreatePage, MonsterModel>(this, "Create", async (obj, data) =>
+            {
+                await CreateAsync(data as MonsterModel);
+            });
+
+            // Register the Update Message
+            MessagingCenter.Subscribe<CharacterUpdatePage, MonsterModel>(this, "Update", async (obj, data) =>
+            {
+                // Have the character update itself
+                data.Update(data);
+
+                await UpdateAsync(data as MonsterModel);
+            });
+
+            // Register the Delete Message
+            MessagingCenter.Subscribe<CharacterDeletePage, MonsterModel>(this, "Delete", async (obj, data) =>
+            {
+                await DeleteAsync(data as MonsterModel);
+            });
+
+            // Register the Set Data Source Message
+            MessagingCenter.Subscribe<AboutPage, int>(this, "SetDataSource", async (obj, data) =>
+            {
+                await SetDataSource(data);
+            });
+
+            // Register the Wipe Data List Message
+            MessagingCenter.Subscribe<AboutPage, bool>(this, "WipeDataList", async (obj, data) =>
+            {
+                await WipeDataListAsync();
+            });
+
+            #endregion Messages
+        }
+
+        #endregion Constructor
+
+        #region DataOperations_CRUDi
+
+        /// <summary>
+        /// Returns the character passed in
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public override MonsterModel CheckIfExists(MonsterModel data)
+        {
+            if (data == null)
+            {
+                return null;
+            }
+
+            // This will walk the characters and find if there is one that is the same.
+            // If so, it returns the character...
+            var myList = Dataset.Where(a =>
+                                        a.Name == data.Name &&
+                                        a.Description == data.Description
+                                        )
+                                        .FirstOrDefault();
+
+            if (myList == null)
+            {
+                // it's not a match, return false;
+                return null;
+            }
+
+            return myList;
+        }
+
+        /// <summary>
+        /// Load the Default Data
+        /// </summary>
+        /// <returns></returns>
+        public override List<MonsterModel> GetDefaultData()
+        {
+            return DefaultData.LoadData(new MonsterModel());
+        }
+
+        #endregion DataOperations_CRUDi
+
+        #region SortDataSet
+
+        /// <summary>
+        /// The Sort Order for the MonsterModel
+        /// </summary>
+        /// <param name="dataset"></param>
+        /// <returns></returns>
+        public override List<MonsterModel> SortDataset(List<MonsterModel> dataset)
+        {
+            return dataset
+                    .OrderBy(a => a.Name)
+                    .ThenBy(a => a.Description)
+                    .ToList();
+        }
+
+        #endregion SortDataSet
+    }
+}
