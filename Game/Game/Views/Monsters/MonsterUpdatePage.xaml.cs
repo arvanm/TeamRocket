b610da8a5ddda5823c790f0547ce972bc2b83299
Yu Fu <yf23@outlook.com>
2021-02-15 16:51:59 -0800

    Remove level and hp, add special attack in MonsterUpdatePage.xaml and MonsterUpdatePage.xaml.cs

--- Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
@@ -37,8 +37,6 @@ namespace Game.Views
 
             this.ViewModel.Title = "Monster Update";
 
-            LoadMonsterLevelPickerValues();
-
             UpdatePageBindingContext();
         }
 
@@ -58,27 +56,10 @@ namespace Game.Views
 
             BindingContext = this.ViewModel;
 
-            // This resets the Picker to the monster's level
-            MonsterLevelPicker.SelectedIndex = ViewModel.Data.Level - 1;
-
             // Sets the Job Picker to the monster's Type
             MonsterTypePicker.SelectedItem = ViewModel.Data.MonsterType.ToMessage();
 
             return true;
-        }
-
-        /// <summary>
-        /// Change the attribute values after changing level in the Level Picker
-        /// </summary>
-        public void UpdateAttributeValues()
-        {
-            // Update the attributes
-            MaxHealthValue.Text = ViewModel.Data.MaxHealth.ToString();
-            CurrentHealthValue.Text = ViewModel.Data.CurrentHealth.ToString();
-            AttackValue.Text = ViewModel.Data.Attack.ToString();
-            DefenseValue.Text = ViewModel.Data.Defense.ToString();
-            SpeedValue.Text = ViewModel.Data.Speed.ToString();
-            TotalAttackValue.Text = ViewModel.Data.GetAttackTotal.ToString();
         }
 
         /// <summary>
@@ -157,65 +138,6 @@ namespace Game.Views
         {
             await Navigation.PopModalAsync();
         }
-
-        #region LevelPicker
-        /// <summary>
-        /// Load the values for the Level Picker
-        /// </summary>
-        /// <returns></returns>
-        public bool LoadMonsterLevelPickerValues()
-        {
-            // Load the values for the Level into the Picker
-            for (var i = 1; i <= LevelTableHelper.MaxLevel; i++)
-            {
-                MonsterLevelPicker.Items.Add(i.ToString());
-            }
-
-            MonsterLevelPicker.SelectedIndex = -1;
-
-            return true;
-        }
-
-        /// <summary>
-        /// The Level selected from the list
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-        public void MonsterLevelPicker_Changed(object sender, EventArgs args)
-        {
-            // If the Picker is not set, then set it
-            if (MonsterLevelPicker.SelectedIndex == -1)
-            {
-                MonsterLevelPicker.SelectedIndex = ViewModel.Data.Level - 1;
-                return;
-            }
-
-            var result = MonsterLevelPicker.SelectedIndex + 1;
-
-            // When level changed, roll again for max health, and set attributes to follow the Level Table
-            if (result != ViewModel.Data.Level)
-            {
-                // Change the Level
-                ViewModel.Data.Level = result;
-
-                // Roll for new max health
-                var oldMaxHealth = ViewModel.Data.MaxHealth;
-                ViewModel.Data.MaxHealth = RandomPlayerHelper.GetHealth(ViewModel.Data.Level);
-
-                // Add the different between old and new max health to the current health
-                // Set a lower limit 1 to avoid the current health drop to too low
-                ViewModel.Data.CurrentHealth = Math.Max(1, ViewModel.Data.CurrentHealth + ViewModel.Data.MaxHealth - oldMaxHealth);
-
-                // Set attack, defense, speed to follow the Level Table
-                ViewModel.Data.Attack = LevelTableHelper.LevelDetailsList[result].Attack;
-                ViewModel.Data.Defense = LevelTableHelper.LevelDetailsList[result].Defense;
-                ViewModel.Data.Speed = LevelTableHelper.LevelDetailsList[result].Speed;
-
-                // Update attribute values in the table
-                UpdateAttributeValues();
-            }
-        }
-        #endregion LevelPicker
 
     }
 }
