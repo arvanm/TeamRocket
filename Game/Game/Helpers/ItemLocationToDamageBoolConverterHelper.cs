8831ba9a4ab789398e263d155a398ff297d715ae
Yu Fu <yf23@outlook.com>
2021-02-17 10:30:39 -0800

    Add ItemLocationToDamageBoolConverterHelper.cs to convert ItemLocationEnum to Boolean that whether it adds damage. Use it in ItemIndexPage.xaml to show damage label.

---
+++ Game/Game/Helpers/ItemLocationToDamageBoolConverterHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+using Game.Models;
+
+namespace Game.Helpers
+{ 
+    // This converter is changing ItemLocationEnum to a boolean representing whether it has damage
+    // Only primary hand and Pokeball have damaes
+
+    // Converts a ItemLocationEnum to Boolean. If ItemLocation is PrimaryHand or Pokeball, return True. Otherwise False.
+    public class ItemLocationToDamageBoolConverterHelper : IValueConverter
+    {
+        /// <summary>
+        /// Converts a ItemLocationEnum to Boolean
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Enum)
+            {
+                return ((ItemLocationEnum)value == ItemLocationEnum.PrimaryHand || (ItemLocationEnum)value == ItemLocationEnum.Pokeball);
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Convert Back, throw not implemented error
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
