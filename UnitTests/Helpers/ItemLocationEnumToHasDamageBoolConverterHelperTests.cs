a5f8a1b509b7393a43c3afc4e78625a434a9b485
Yu Fu <yf23@outlook.com>
2021-02-22 13:27:15 -0800

    Add test on ItemLocationEnumToHadDamageBoolConverterHelperTests.cs

---
+++ UnitTests/Helpers/ItemLocationEnumToHasDamageBoolConverterHelperTests.cs
@@ -0,0 +1,95 @@
+﻿using NUnit.Framework;
+
+using Game.Services;
+using Game.Models;
+using Game.Helpers;
+using System;
+using System.Linq;
+
+namespace UnitTests.Helpers
+{
+    [TestFixture]
+    public class ItemLocationEnumToHasDamageBoolConverterHelperTests
+    {
+
+        [Test]
+        public void ItemLocationEnumToHasDamageBoolConverterHelper_Convert_ItemLocationEnum_PrimaryHand_Should_Retrun_True()
+        {
+            // Arrange
+            var myConverter = new ItemLocationToHasDamageBoolConverterHelper();
+            var myObject = ItemLocationEnum.PrimaryHand;
+
+            // Act
+            var result = myConverter.Convert(myObject, null, null, null);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(result, true);
+        }
+
+        [Test]
+        public void ItemLocationEnumToHasDamageBoolConverterHelper_Convert_ItemLocationEnum_Pokeball_Should_Return_True()
+        {
+            // Arrange
+            var myConverter = new ItemLocationToHasDamageBoolConverterHelper();
+            var myObject = ItemLocationEnum.Pokeball;
+
+            // Act
+            var result = myConverter.Convert(myObject, null, null, null);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(result, true);
+        }
+
+        [Test]
+        public void ItemLocationEnumToHasDamageBoolConverterHelper_Convert_ItemLocationEnum_Head_Should_Return_False()
+        {
+            // Arrange
+            var myConverter = new ItemLocationToHasDamageBoolConverterHelper();
+            var myObject = ItemLocationEnum.Head;
+
+            // Act
+            var result = myConverter.Convert(myObject, null, null, null);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(result, false);
+        }
+
+        [Test]
+        public void ItemLocationEnumToHasDamageBoolConverterHelper_Convert_Other_Should_Skip()
+        {
+            // Arrange
+            var myConverter = new ItemLocationToHasDamageBoolConverterHelper();
+            var myObject = new ItemModel();
+
+            // Act
+            var result = myConverter.Convert(myObject, null, null, null);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(result, 0);
+        }
+
+        //[Test]
+        //[ExpectedException("System.NotImplementedException")]
+        //public void ItemLocationEnumToHasDamageBoolConverterHelper_ConvertBack_Should_Throw_Exception()
+        //{
+        //    // Arrange
+        //    var myConverter = new ItemLocationToHasDamageBoolConverterHelper();
+        //    var myObject = true;
+
+        //    // Act
+        //    var result = myConverter.ConvertBack(myObject, null, null, null);
+
+        //    // Reset
+
+        //    // Assert
+        //}
+    }
+}
