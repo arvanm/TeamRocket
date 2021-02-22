327090b58b366824754e68aa4d262e3989498b59
Yu Fu <yf23@outlook.com>
2021-02-22 13:29:22 -0800

    Add tests in ItemLocationEnumToHasRangeBoolConverterHelperTests.cs

---
+++ UnitTests/Helpers/ItemLocationEnumToHasRangeBoolConverterHelperTests.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+
+using Game.Services;
+using Game.Models;
+using Game.Helpers;
+using System;
+using System.Linq;
+
+namespace UnitTests.Helpers
+{
+    [TestFixture]
+    public class ItemLocationEnumToHasRangeBoolConverterHelperTests
+    {
+
+        [Test]
+        public void ItemLocationEnumToHasRangeBoolConverterHelper_Convert_ItemLocationEnum_PrimaryHand_Should_Retrun_True()
+        {
+            // Arrange
+            var myConverter = new ItemLocationToHasRangeBoolConverterHelper();
+            var myObject = ItemLocationEnum.PrimaryHand;
+
+            // Act
+            var result = myConverter.Convert(myObject, null, null, null);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(result, true);
+        }
+
+        [Test]
+        public void ItemLocationEnumToHasRangeBoolConverterHelper_Convert_ItemLocationEnum_Head_Should_Return_False()
+        {
+            // Arrange
+            var myConverter = new ItemLocationToHasRangeBoolConverterHelper();
+            var myObject = ItemLocationEnum.Head;
+
+            // Act
+            var result = myConverter.Convert(myObject, null, null, null);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(result, false);
+        }
+
+        [Test]
+        public void ItemLocationEnumToHasRangeBoolConverterHelper_Convert_Other_Should_Skip()
+        {
+            // Arrange
+            var myConverter = new ItemLocationToHasRangeBoolConverterHelper();
+            var myObject = new ItemModel();
+
+            // Act
+            var result = myConverter.Convert(myObject, null, null, null);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(result, 0);
+        }
+
+        //[Test]
+        //[ExpectedException("System.NotImplementedException")]
+        //public void ItemLocationEnumToHasRangeBoolConverterHelper_ConvertBack_Should_Throw_Exception()
+        //{
+        //    // Arrange
+        //    var myConverter = new ItemLocationToHasRangeBoolConverterHelper();
+        //    var myObject = true;
+
+        //    // Act
+        //    var result = myConverter.ConvertBack(myObject, null, null, null);
+
+        //    // Reset
+
+        //    // Assert
+        //}
+    }
+}
