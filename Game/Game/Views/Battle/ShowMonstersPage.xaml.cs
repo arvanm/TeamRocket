87f0bb4bc932b065e1dc241a6137e56f2afbd8c3
Michael Arvan <arvanmichael@seattleu.edu>
2021-02-24 14:58:49 -0800

    Paritial - Add ShowMonstersPage(s) to project

---
+++ Game/Game/Views/Battle/ShowMonstersPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Game.Models;
+using Game.ViewModels;
+using System.Linq;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Selecting Characters for the Game
+    /// 
+    /// TODO: Team
+    /// Mike's game allows duplicate characters in a party (6 Mikes can all fight)
+    /// If you do not allow duplicates, change the code below
+    /// Instead of using the database list directly make a copy of it in the viewmodel
+    /// Then have on select of the database remove the character from that list and add to the part list
+    /// Have select from the party list remove it from the party list and add it to the database list
+    ///
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "<Pending>")]
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShowMonstersPage : ContentPage
+    {
+        // The view model, used for data binding
+        readonly MonsterIndexViewModel ViewModel = MonsterIndexViewModel.Instance;
+        // Empty Constructor for UTs
+        public ShowMonstersPage(bool UnitTest) { }
+
+        /// <summary>
+        /// Constructor for Index Page
+        /// 
+        /// Get the MonsterIndexView Model
+        /// </summary>
+        public ShowMonstersPage()
+        {
+            InitializeComponent();
+
+            BindingContext = BattleEngineViewModel.Instance;
+            //BindingContext = BattleEngineViewModel.Instance;
+
+            // Clear the Database List and the Party List to start
+            BattleEngineViewModel.Instance.PartyCharacterList.Clear();
+
+           // UpdateNextButtonState();
+        }
+
+
+
+        /// <summary>
+        /// Jump to the Battle
+        /// 
+        /// Its Modal because don't want user to come back...
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void BattleButton_Clicked(object sender, EventArgs e)
+        {
+            CreateEngineCharacterList();
+
+            //await Navigation.PushModalAsync(new NavigationPage(new ShowMonsterPage()));
+
+            await Navigation.PushModalAsync(new NavigationPage(new BattlePage()));
+            await Navigation.PopAsync();
+        }
+
+        /// <summary>
+        /// Clear out the old list and make the new list
+        /// </summary>
+        public void CreateEngineCharacterList()
+        {
+            // Clear the currett list
+            BattleEngineViewModel.Instance.Engine.EngineSettings.CharacterList.Clear();
+
+            // Load the Characters into the Engine
+            foreach (var data in BattleEngineViewModel.Instance.PartyCharacterList)
+            {
+                data.CurrentHealth = data.GetMaxHealthTotal;
+                BattleEngineViewModel.Instance.Engine.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
+            }
+        }
+    }
+}
