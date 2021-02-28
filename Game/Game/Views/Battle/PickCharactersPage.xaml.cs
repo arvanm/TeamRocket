503460adac40ee542eedffa955da5c5e734195f4
Yu Fu <yf23@outlook.com>
2021-02-28 00:21:27 -0800

    Change Go button in PickCharactersPage.xaml.cs to navigate to start round page. Change PickCharactersPage.xaml.cs

--- Game/Game/Views/Battle/PickCharactersPage.xaml.cs
+++ Game/Game/Views/Battle/PickCharactersPage.xaml.cs
@@ -148,9 +148,8 @@ namespace Game.Views
         {
             CreateEngineCharacterList();
 
-            await Navigation.PushModalAsync(new NavigationPage(new ShowMonstersPage()));
+            await Navigation.PushModalAsync(new NavigationPage(new NewRoundPage()));
 
-            //await Navigation.PushModalAsync(new NavigationPage(new BattlePage()));
             await Navigation.PopAsync();
         }
 
