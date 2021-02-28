c3e8bfae5bde094860c28d287f5ed96379bb2942
Yu Fu <yf23@outlook.com>
2021-02-28 12:53:37 -0800

    Change the layout of NewRoundPage. Changes NewRoundPage.xaml, NewRoundPage.xaml.cs, App.xaml

--- Game/Game/Views/Battle/NewRoundPage.xaml.cs
+++ Game/Game/Views/Battle/NewRoundPage.xaml.cs
@@ -9,53 +9,53 @@ using Game.ViewModels;
 
 namespace Game.Views
 {
-	/// <summary>
-	/// The Main Game Page
-	/// </summary>
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class NewRoundPage: ContentPage
-	{
-		// This uses the Instance so it can be shared with other Battle Pages as needed
-		public BattleEngineViewModel EngineViewModel = BattleEngineViewModel.Instance;
+    /// <summary>
+    /// The Main Game Page
+    /// </summary>
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewRoundPage : ContentPage
+    {
+        // This uses the Instance so it can be shared with other Battle Pages as needed
+        public BattleEngineViewModel EngineViewModel = BattleEngineViewModel.Instance;
 
-		/// <summary>
-		/// Constructor
-		/// </summary>
-		public NewRoundPage ()
-		{
-			InitializeComponent ();
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public NewRoundPage()
+        {
+            InitializeComponent();
 
-			// Draw the Characters
-			foreach (var data in EngineViewModel.Engine.EngineSettings.CharacterList)
-			{
+            // Draw the Characters
+            foreach (var data in EngineViewModel.Engine.EngineSettings.CharacterList)
+            {
                 PartyListFrame.Children.Add(CreatePlayerDisplayBox(data));
-			}
+            }
 
-			// Draw the Monsters
-			foreach (var data in EngineViewModel.Engine.EngineSettings.MonsterList)
-			{
-				MonsterListFrame.Children.Add(CreatePlayerDisplayBox(data));
-			}
+            // Draw the Monsters
+            foreach (var data in EngineViewModel.Engine.EngineSettings.MonsterList)
+            {
+                MonsterListFrame.Children.Add(CreatePlayerDisplayBox(data));
+            }
 
-		}
+        }
 
-		/// <summary>
-		/// Start next Round, returning to the battle screen
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		public async void BeginButton_Clicked(object sender, EventArgs e)
-		{
-			await Navigation.PopModalAsync();
-		}
+        /// <summary>
+        /// Start next Round, returning to the battle screen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void BeginButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
 
-		/// <summary>
-		/// Return a stack layout with the Player information inside
-		/// </summary>
-		/// <param name="data"></param>
-		/// <returns></returns>
-		public StackLayout CreatePlayerDisplayBox(PlayerInfoModel data)
-		{
+        /// <summary>
+        /// Return a stack layout with the Player information inside
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public StackLayout CreatePlayerDisplayBox(PlayerInfoModel data)
+        {
             if (data == null)
             {
                 data = new PlayerInfoModel();
@@ -71,13 +71,13 @@ namespace Game.Views
             // Add the Level
             var PlayerLevelLabel = new Label
             {
-                Text = "Level : "+data.Level,
+                Text = "Level : " + data.Level,
                 Style = (Style)Application.Current.Resources["ValueStyleMicro"],
                 HorizontalOptions = LayoutOptions.Center,
                 HorizontalTextAlignment = TextAlignment.Center,
                 Padding = 0,
                 LineBreakMode = LineBreakMode.TailTruncation,
-                CharacterSpacing = 1,
+                CharacterSpacing = 0,
                 LineHeight = 1,
                 MaxLines = 1,
             };
@@ -85,17 +85,18 @@ namespace Game.Views
             // Add the HP
             var PlayerHPLabel = new Label
             {
-                Text = "HP : "+ data.GetCurrentHealthTotal,
+                Text = "HP : " + data.GetCurrentHealthTotal,
                 Style = (Style)Application.Current.Resources["ValueStyleMicro"],
                 HorizontalOptions = LayoutOptions.Center,
                 HorizontalTextAlignment = TextAlignment.Center,
                 Padding = 0,
                 LineBreakMode = LineBreakMode.TailTruncation,
-                CharacterSpacing = 1,
+                CharacterSpacing = 0,
                 LineHeight = 1,
                 MaxLines = 1,
             };
 
+            // Add the Name
             var PlayerNameLabel = new Label()
             {
                 Text = data.Name,
@@ -103,10 +104,22 @@ namespace Game.Views
                 HorizontalOptions = LayoutOptions.Center,
                 HorizontalTextAlignment = TextAlignment.Center,
                 Padding = 0,
-                LineBreakMode = LineBreakMode.TailTruncation,
-                CharacterSpacing=1,
-                LineHeight=1,
-                MaxLines =1,
+                CharacterSpacing = 0,
+                LineHeight = 1,
+                MaxLines = 1,
+            };
+
+            // Add the character job or monster type
+            var PlayerTypeLabel = new Label()
+            {
+                Text = (data.PlayerType == PlayerTypeEnum.Character) ? data.Job.ToMessage() : data.MonsterType.ToMessage(),
+                Style = (Style)Application.Current.Resources["ValueStyle"],
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
+                Padding = 0,
+                CharacterSpacing = 0,
+                LineHeight = 1,
+                MaxLines = 1,
             };
 
             // Put the Image Button and Text inside a layout
@@ -116,15 +129,17 @@ namespace Game.Views
                 HorizontalOptions = LayoutOptions.Center,
                 Padding = 0,
                 Spacing = 0,
+                Margin = 5,
                 Children = {
                     PlayerImage,
                     PlayerNameLabel,
+                    PlayerTypeLabel,
                     PlayerLevelLabel,
                     PlayerHPLabel,
                 },
             };
 
             return PlayerStack;
-		}
-	}
+        }
+    }
 }
