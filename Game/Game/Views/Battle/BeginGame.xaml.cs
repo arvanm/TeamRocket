113a82cc4a923bfa4b4e2800424f755821a5a74e
Yu Fu <yf23@outlook.com>
2021-02-28 02:43:56 -0800

    Create a game begin page. Creates BeginGame.xaml and BeginGame.xaml.cs. Changes project file

---
+++ Game/Game/Views/Battle/BeginGame.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Game.Models;
+using Game.ViewModels;
+using System;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views
+{
+	/// <summary>
+	/// The Main Game Page
+	/// </summary>
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BeginGame: ContentPage
+	{
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public BeginGame()
+        {
+            InitializeComponent();
+        }
+
+        private async void StartGame_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PickCharactersPage());
+        }
+    }
+}
