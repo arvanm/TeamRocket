49ed10b7afcdea95a32b23fc45f8964463c82a79
Yu Fu <yf23@outlook.com>
2021-03-08 14:57:26 -0800

    Add UTs for RoundEngineGame. Changes RoundEngineGameTests.cs

--- UnitTests/Engine/EngineGame/RoundEngineGameTests.cs
+++ UnitTests/Engine/EngineGame/RoundEngineGameTests.cs
@@ -49,5 +49,80 @@ namespace UnitTests.Engine.EngineGame
             Assert.IsNotNull(result);
         }
         #endregion Constructor
+
+        #region RoundNextTurn
+        [Test]
+        public void RoundEngine_RoundNextTurn_Valid_No_Characters_Should_Return_GameOver()
+        {
+            Engine.EngineSettings.MonsterList.Clear();
+
+            // Arrange
+            var Character = new CharacterModel
+            {
+                Speed = 20,
+                Level = 1,
+                CurrentHealth = 1,
+                ExperienceTotal = 1,
+                Name = "Characer",
+                ListOrder = 1,
+            };
+
+            // Add each model here to warm up and load it.
+            Game.Helpers.DataSetsHelper.WarmUp();
+
+            Engine.EngineSettings.CharacterList.Clear();
+
+            Engine.EngineSettings.MonsterList.Add(new PlayerInfoModel(Character));
+
+            // Make the List
+            Engine.EngineSettings.PlayerList = Engine.Round.MakePlayerList();
+
+            // Act
+            var result = Engine.Round.RoundNextTurn();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(RoundEnum.GameOver, result);
+        }
+
+        [Test]
+        public void RoundEngine_RoundNextTurn_Valid_No_Monsters_Should_Return_NewRound()
+        {
+            Engine.EngineSettings.MonsterList.Clear();
+
+            // Arrange
+            var Character = new CharacterModel
+            {
+                Speed = 20,
+                Level = 1,
+                CurrentHealth = 1,
+                ExperienceTotal = 1,
+                Name = "Characer",
+                ListOrder = 1,
+            };
+
+            // Add each model here to warm up and load it.
+            Game.Helpers.DataSetsHelper.WarmUp();
+
+            Engine.EngineSettings.CharacterList.Clear();
+
+            Engine.EngineSettings.CharacterList.Add(new PlayerInfoModel(Character));
+
+            //Engine.EngineSettings.MonsterList.Add(new PlayerInfoModel(Character));
+
+            // Make the List
+            Engine.EngineSettings.PlayerList = Engine.Round.MakePlayerList();
+
+            // Act
+            var result = Engine.Round.RoundNextTurn();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(RoundEnum.NewRound, result);
+        }
+
+        #endregion RoundNextTurn
     }
 }
