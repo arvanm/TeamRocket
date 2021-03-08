c491ba4b42bb01c604fc4e3e19312bc8156710eb
Yu Fu <yf23@outlook.com>
2021-03-08 11:00:42 -0800

    Add update method from PlayerInforModel for CharacterModel.cs

--- Game/Game/Models/CharacterModel.cs
+++ Game/Game/Models/CharacterModel.cs
@@ -96,6 +96,63 @@ namespace Game.Models
         }
 
         /// <summary>
+        /// Create a copy
+        /// </summary>
+        /// <param name="data"></param>
+        public CharacterModel(PlayerInfoModel data)
+        {
+            Update(data);
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        /// <param name="newData"></param>
+        /// <returns></returns>
+        public bool Update(PlayerInfoModel newData)
+        {
+            if (newData == null)
+            {
+                return false;
+            }
+
+            PlayerType = newData.PlayerType;
+            Guid = newData.Guid;
+            Name = newData.Name;
+            Description = newData.Description;
+            Level = newData.Level;
+            ImageURI = newData.ImageURI;
+
+            // Difficulty = newData.Difficulty;
+            // Speed = newData.Speed;
+            // Defense = newData.Defense;
+            // Attack = newData.Attack;
+
+            ExperienceTotal = newData.ExperienceTotal;
+            ExperienceRemaining = newData.ExperienceRemaining;
+            CurrentHealth = newData.CurrentHealth;
+            MaxHealth = newData.MaxHealth;
+
+            Head = newData.Head;
+            Necklass = newData.Necklass;
+            PrimaryHand = newData.PrimaryHand;
+            OffHand = newData.OffHand;
+            RightFinger = newData.RightFinger;
+            LeftFinger = newData.LeftFinger;
+            Feet = newData.Feet;
+            UniqueItem = newData.UniqueItem;
+            Pokeball = newData.Pokeball;
+
+            // Update the Job
+            Job = newData.Job;
+
+            // Update the Pokedex
+            Pokedex = newData.Pokedex;
+
+            return true;
+        }
+
+        /// <summary>
         /// Helper to combine the attributes into a single line, to make it easier to display the item as a string
         /// </summary>
         /// <returns></returns>
