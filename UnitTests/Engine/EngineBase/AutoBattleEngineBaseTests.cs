b1629517afa7b771f05b100cf34f188502d3edc6
Yu Fu <yf23@outlook.com>
2021-03-08 22:28:47 -0800

    Change character to weaker in AutoBattleEngineBaseTests.cs

--- UnitTests/Engine/EngineBase/AutoBattleEngineBaseTests.cs
+++ UnitTests/Engine/EngineBase/AutoBattleEngineBaseTests.cs
@@ -116,7 +116,7 @@ namespace UnitTests.Engine.EngineBase
                             {
                                 Speed = -1,
                                 Level = 10,
-                                CurrentHealth = 11,
+                                CurrentHealth = 1,
                                 ExperienceTotal = 1,
                                 ExperienceRemaining = 1,
                                 Name = "Mike",
