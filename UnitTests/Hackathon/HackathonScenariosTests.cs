9a7c01bded5a6aefe4b7224f631fc0960ed51118
Yu Fu <yf23@outlook.com>
2021-03-13 02:29:58 -0800

    Add more test cases for Hackathon Scenario 9 Item Delivery, Changes HackathonScenariosTests.cs

--- UnitTests/Hackathon/HackathonScenariosTests.cs
+++ UnitTests/Hackathon/HackathonScenariosTests.cs
@@ -148,6 +148,8 @@ namespace Scenario
             var result = await EngineViewModel.AutoBattleEngine.RunAutoBattle();
 
             //Reset
+            EngineViewModel.Engine.EngineSettings.CharacterList.Clear();
+            EngineViewModel.Engine.EngineSettings.PlayerList.Clear();
             EngineViewModel.Engine.EngineSettings.BattleSettingsModel.MonsterHitEnum = HitStatusEnum.Default;
             DiceHelper.DisableForcedRolls();
 
@@ -218,6 +220,8 @@ namespace Scenario
             var result = await EngineViewModel.AutoBattleEngine.RunAutoBattle();
 
             //Reset
+            EngineViewModel.Engine.EngineSettings.CharacterList.Clear();
+            EngineViewModel.Engine.EngineSettings.PlayerList.Clear();
             EngineViewModel.Engine.EngineSettings.BattleSettingsModel.MonsterHitEnum = HitStatusEnum.Default;
 
             //Assert
@@ -248,10 +252,10 @@ namespace Scenario
             *      Run Auto Battle
             * 
             * Test Conditions:
-            *      Head Item the test character have
+            *      head item the test character have
             * 
             * Validation:
-            *      Test character should have Head 
+            *      should not be null
             */
 
             //Arrange
@@ -292,15 +296,233 @@ namespace Scenario
 
             //Act
             var result = await EngineViewModel.AutoBattleEngine.RunAutoBattle();
-            var headItem = EngineViewModel.Engine.EngineSettings.PlayerList.Where(m => m.Name == "Test").FirstOrDefault().DropAllItems().Where(m => m.Location == ItemLocationEnum.Head).FirstOrDefault();
+            var NewHeadItem = EngineViewModel.Engine.EngineSettings.PlayerList.Where(m => m.Name == "Test").FirstOrDefault().DropAllItems().Where(m => m.Location == ItemLocationEnum.Head).FirstOrDefault();
 
             //Reset
+            EngineViewModel.Engine.EngineSettings.CharacterList.Clear();
+            EngineViewModel.Engine.EngineSettings.PlayerList.Clear();
             EngineViewModel.Engine.EngineSettings.MaxRoundCount = 100;
             EngineViewModel.Engine.EngineSettings.BattleSettingsModel.MonsterHitEnum = HitStatusEnum.Default;
             EngineViewModel.Engine.EngineSettings.AmazonDeliver = false;
+            await ItemIndexViewModel.Instance.DeleteAsync(NewHeadItem);
 
             //Assert
-            Assert.NotNull(headItem);
+            Assert.NotNull(NewHeadItem);
+        }
+
+        [Test]
+        public async Task HackathonScenario_Scenario_9_Valid_Bad_Primary_Hand_Should_Improved()
+        {
+            /* 
+            * Scenario Number:  
+            *      9
+            *      
+            * Description: 
+            *      9.	Just in Time Delivery
+            * 
+            * Changes Required (Classes, Methods etc.) :
+            *      Modifies EndRound() in RoundEngine.cs
+            * 
+            * Test Algrorithm:
+            *      Add one character with 8 items, all with value 20 except for Primary hand, which is 19
+            *      Run 1 round to make sure better primary hand delivers
+            *      Startup Battle
+            *      Run Auto Battle
+            * 
+            * Test Conditions:
+            *      Value of Primary Hand Item the test character 
+            * 
+            * Validation:
+            *      Test character should have primary hand with value 20
+            */
+
+            //Arrange
+
+            // Max Character Size is 1
+            EngineViewModel.Engine.EngineSettings.MaxNumberPartyCharacters = 1;
+
+            // Make sure all items (except Pokeball) are delivered
+            EngineViewModel.Engine.EngineSettings.MaxRoundCount = 1;
+
+            // Monster always miss
+            EngineViewModel.Engine.EngineSettings.BattleSettingsModel.MonsterHitEnum = HitStatusEnum.Miss;
+
+            // Amazon delivers
+            EngineViewModel.Engine.EngineSettings.AmazonDeliver = true;
+
+            // Add Items
+            var FeetItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.Feet, Attribute = AttributeEnum.Attack, Value = 20 };
+            var NeckItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.Necklass, Attribute = AttributeEnum.Attack, Value = 20 };
+            var PHandItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.PrimaryHand, Attribute = AttributeEnum.Attack, Value = 19 };
+            var OHandItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.OffHand, Attribute = AttributeEnum.Attack, Value = 20 };
+            var LFingerItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.Finger, Attribute = AttributeEnum.Attack, Value = 20 };
+            var RFingerItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.Finger, Attribute = AttributeEnum.Attack, Value = 20 };
+            var PokeballItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.Pokeball, Attribute = AttributeEnum.Attack, Value = 20 };
+            var HeadItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.Head, Attribute = AttributeEnum.Attack, Value = 20 };
+            await ItemIndexViewModel.Instance.CreateAsync(FeetItem);
+            await ItemIndexViewModel.Instance.CreateAsync(NeckItem);
+            await ItemIndexViewModel.Instance.CreateAsync(PHandItem);
+            await ItemIndexViewModel.Instance.CreateAsync(OHandItem);
+            await ItemIndexViewModel.Instance.CreateAsync(LFingerItem);
+            await ItemIndexViewModel.Instance.CreateAsync(RFingerItem);
+            await ItemIndexViewModel.Instance.CreateAsync(PokeballItem);
+            await ItemIndexViewModel.Instance.CreateAsync(HeadItem);
+            ItemIndexViewModel.Instance.SetNeedsRefresh(true);
+
+            // Add Character
+            var CharacterPlayer = new PlayerInfoModel(new CharacterModel
+            {
+                Speed = 10,
+                Level = 20,
+                CurrentHealth = 1000,
+                ExperienceTotal = 4,
+                ExperienceRemaining = 5,
+                Name = "Test",
+                Feet = FeetItem.Id,
+                Necklass = NeckItem.Id,
+                PrimaryHand = PHandItem.Id,
+                OffHand = OHandItem.Id,
+                LeftFinger = LFingerItem.Id,
+                RightFinger = RFingerItem.Id,
+                Pokeball = PokeballItem.Id,
+                Head = HeadItem.Id
+            });
+
+            EngineViewModel.Engine.EngineSettings.CharacterList.Add(CharacterPlayer);
+            EngineViewModel.Engine.EngineSettings.PlayerList.Add(CharacterPlayer);
+
+            //Act
+            await EngineViewModel.AutoBattleEngine.RunAutoBattle();
+            var NewPrimaryHandItem = EngineViewModel.Engine.EngineSettings.PlayerList.Where(m => m.Name == "Test").FirstOrDefault().DropAllItems().Where(m => m.Location == ItemLocationEnum.PrimaryHand).FirstOrDefault();
+
+            //Reset
+            EngineViewModel.Engine.EngineSettings.CharacterList.Clear();
+            EngineViewModel.Engine.EngineSettings.PlayerList.Clear();
+            EngineViewModel.Engine.EngineSettings.MaxRoundCount = 100;
+            EngineViewModel.Engine.EngineSettings.BattleSettingsModel.MonsterHitEnum = HitStatusEnum.Default;
+            EngineViewModel.Engine.EngineSettings.AmazonDeliver = false;
+            await ItemIndexViewModel.Instance.DeleteAsync(FeetItem);
+            await ItemIndexViewModel.Instance.DeleteAsync(NeckItem);
+            await ItemIndexViewModel.Instance.DeleteAsync(PHandItem);
+            await ItemIndexViewModel.Instance.DeleteAsync(OHandItem);
+            await ItemIndexViewModel.Instance.DeleteAsync(LFingerItem);
+            await ItemIndexViewModel.Instance.DeleteAsync(RFingerItem);
+            await ItemIndexViewModel.Instance.DeleteAsync(PokeballItem);
+            await ItemIndexViewModel.Instance.DeleteAsync(HeadItem);
+            await ItemIndexViewModel.Instance.DeleteAsync(NewPrimaryHandItem);
+
+            //Assert
+            Assert.AreEqual(NewPrimaryHandItem.Value, 20);
+        }
+
+        [Test]
+        public async Task HackathonScenario_Scenario_9_Valid_Perfect_Items_Should_Not_Deliver()
+        {
+            /* 
+            * Scenario Number:  
+            *      9
+            *      
+            * Description: 
+            *      9.	Just in Time Delivery
+            * 
+            * Changes Required (Classes, Methods etc.) :
+            *      Modifies EndRound() in RoundEngine.cs
+            * 
+            * Test Algrorithm:
+            *      Add one character with 8 items, all with value 20
+            *      Run 1 round to try to have deliver
+            *      Startup Battle
+            *      Run Auto Battle
+            * 
+            * Test Conditions:
+            *      Number of items in ItemsViewModel
+            * 
+            * Validation:
+            *      Number of items in ItemsViewModel should not change
+            */
+
+            //Arrange
+
+            // Max Character Size is 1
+            EngineViewModel.Engine.EngineSettings.MaxNumberPartyCharacters = 1;
+
+            // Make sure all items (except Pokeball) are delivered
+            EngineViewModel.Engine.EngineSettings.MaxRoundCount = 1;
+
+            // Monster always miss
+            EngineViewModel.Engine.EngineSettings.BattleSettingsModel.MonsterHitEnum = HitStatusEnum.Miss;
+
+            // Amazon delivers
+            EngineViewModel.Engine.EngineSettings.AmazonDeliver = true;
+
+            // Add Items
+            var FeetItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.Feet, Attribute = AttributeEnum.Attack, Value = 20 };
+            var NeckItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.Necklass, Attribute = AttributeEnum.Attack, Value = 20 };
+            var PHandItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.PrimaryHand, Attribute = AttributeEnum.Attack, Value = 20 };
+            var OHandItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.OffHand, Attribute = AttributeEnum.Attack, Value = 20 };
+            var LFingerItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.Finger, Attribute = AttributeEnum.Attack, Value = 20 };
+            var RFingerItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.Finger, Attribute = AttributeEnum.Attack, Value = 20 };
+            var PokeballItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.Pokeball, Attribute = AttributeEnum.Attack, Value = 20 };
+            var HeadItem = new ItemModel { Name = "Test", Location = ItemLocationEnum.Head, Attribute = AttributeEnum.Attack, Value = 20 };
+            await ItemIndexViewModel.Instance.CreateAsync(FeetItem);
+            await ItemIndexViewModel.Instance.CreateAsync(NeckItem);
+            await ItemIndexViewModel.Instance.CreateAsync(PHandItem);
+            await ItemIndexViewModel.Instance.CreateAsync(OHandItem);
+            await ItemIndexViewModel.Instance.CreateAsync(LFingerItem);
+            await ItemIndexViewModel.Instance.CreateAsync(RFingerItem);
+            await ItemIndexViewModel.Instance.CreateAsync(PokeballItem);
+            await ItemIndexViewModel.Instance.CreateAsync(HeadItem);
+            ItemIndexViewModel.Instance.SetNeedsRefresh(true);
+
+            // Add Character
+            var CharacterPlayer = new PlayerInfoModel(new CharacterModel
+            {
+                Speed = 10,
+                Level = 20,
+                CurrentHealth = 1000,
+                ExperienceTotal = 4,
+                ExperienceRemaining = 5,
+                Name = "Test",
+                Feet = FeetItem.Id,
+                Necklass = NeckItem.Id,
+                PrimaryHand = PHandItem.Id,
+                OffHand = OHandItem.Id,
+                LeftFinger = LFingerItem.Id,
+                RightFinger = RFingerItem.Id,
+                Pokeball = PokeballItem.Id,
+                Head = HeadItem.Id
+            });
+
+            EngineViewModel.Engine.EngineSettings.CharacterList.Add(CharacterPlayer);
+            EngineViewModel.Engine.EngineSettings.PlayerList.Add(CharacterPlayer);
+
+            // Dice for monster drop, set to 0 so monster does not drop items
+            DiceHelper.EnableForcedRolls();
+            DiceHelper.SetForcedRollValue(1);
+
+            //Act
+            var OldItemsCount = ItemIndexViewModel.Instance.Dataset.Count();
+            await EngineViewModel.AutoBattleEngine.RunAutoBattle();
+            var NewItemsCount = ItemIndexViewModel.Instance.Dataset.Count();
+
+            //Reset
+            EngineViewModel.Engine.EngineSettings.CharacterList.Clear();
+            EngineViewModel.Engine.EngineSettings.PlayerList.Clear();
+            EngineViewModel.Engine.EngineSettings.MaxRoundCount = 100;
+            EngineViewModel.Engine.EngineSettings.BattleSettingsModel.MonsterHitEnum = HitStatusEnum.Default;
+            EngineViewModel.Engine.EngineSettings.AmazonDeliver = false;
+            DiceHelper.DisableForcedRolls();
+            await ItemIndexViewModel.Instance.DeleteAsync(FeetItem);
+            await ItemIndexViewModel.Instance.DeleteAsync(NeckItem);
+            await ItemIndexViewModel.Instance.DeleteAsync(PHandItem);
+            await ItemIndexViewModel.Instance.DeleteAsync(OHandItem);
+            await ItemIndexViewModel.Instance.DeleteAsync(LFingerItem);
+            await ItemIndexViewModel.Instance.DeleteAsync(RFingerItem);
+            await ItemIndexViewModel.Instance.DeleteAsync(PokeballItem);
+            await ItemIndexViewModel.Instance.DeleteAsync(HeadItem);
+
+            //Assert
+            Assert.AreEqual(NewItemsCount, OldItemsCount);
         }
         #endregion Scenario9
     }
