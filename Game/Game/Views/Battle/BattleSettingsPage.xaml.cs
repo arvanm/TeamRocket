4ece3f72899da79bf40926af9bcf62c0ffd00ba7
Yu Fu <yf23@outlook.com>
2021-03-16 09:14:56 -0700

    Change BattleSettingsPage to only have AmazonDelivery. Changes BattleSettingsPage.xaml, BattleSettingsPage.xaml.cs, BattleSettingsPageTests.cs

--- Game/Game/Views/Battle/BattleSettingsPage.xaml.cs
+++ Game/Game/Views/Battle/BattleSettingsPage.xaml.cs
@@ -15,7 +15,7 @@ namespace Game.Views
     public partial class BattleSettingsPage : ContentPage
     {
         // Empty Constructor for UTs
-       // public BattleSettingsPage(bool UnitTest) { }
+        // public BattleSettingsPage(bool UnitTest) { }
 
         /// <summary>
         /// Constructor
@@ -24,93 +24,8 @@ namespace Game.Views
         {
             InitializeComponent();
 
-            #region BattleMode
-            // Load the values for the Diffculty into the Picker
-            foreach (var item in BattleModeEnumHelper.GetListMessageAll)
-            {
-                BattleModePicker.Items.Add(item);
-            }
-
             // Set Values to current State
-            BattleModePicker.SelectedItem = BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.BattleModeEnum.ToMessage();
-            BattleModePicker.SelectedIndex = BattleModePicker.Items.IndexOf(BattleModePicker.SelectedItem.ToString());
-            #endregion BattleMode
-
-            #region HitPickers
-            // Load the values for the Diffculty into the Picker
-            foreach (var item in HitStatusEnumHelper.GetListAll)
-            {
-                MonsterHitPicker.Items.Add(item);
-                CharacterHitPicker.Items.Add(item);
-            }
-
-            // Set Values to current State
-            MonsterHitPicker.SelectedItem = BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.MonsterHitEnum.ToString();
-            MonsterHitPicker.SelectedIndex = MonsterHitPicker.Items.IndexOf(MonsterHitPicker.SelectedItem.ToString());
-
-            CharacterHitPicker.SelectedItem = BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.CharacterHitEnum.ToString();
-            CharacterHitPicker.SelectedIndex = CharacterHitPicker.Items.IndexOf(CharacterHitPicker.SelectedItem.ToString());
-            #endregion HitPickers
-
-            #region HitToggles
-            AllowCriticalHitSwitch.IsToggled = BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.AllowCriticalHit;
-            AllowCriticalMissSwitch.IsToggled = BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.AllowCriticalMiss;
-            #endregion HitToggles
-
-            #region MonsterToggles
-            AllowMonsterItemsSwitch.IsToggled = BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.AllowMonsterItems;
-            #endregion
-        }
-
-        /// <summary>
-        /// Set the Character Hit Enum
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-        public void BattleModePicker_Changed(object sender, EventArgs args)
-        {
-            // Check for null, SelectedItem is not set when the control is created
-            if (BattleModePicker.SelectedItem == null)
-            {
-                return;
-            }
-
-            // Change the Difficulty
-            BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.BattleModeEnum = BattleModeEnumHelper.ConvertMessageStringToEnum(BattleModePicker.SelectedItem.ToString());
-        }
-
-        /// <summary>
-        /// Set the Monster Hit Enum
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-        public void MonsterHitPicker_Changed(object sender, EventArgs args)
-        {
-            // Check for null, SelectedItem is not set when the control is created
-            if (MonsterHitPicker.SelectedItem == null)
-            {
-                return;
-            }
-
-            // Change the Difficulty
-            BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.MonsterHitEnum = HitStatusEnumHelper.ConvertStringToEnum(MonsterHitPicker.SelectedItem.ToString());
-        }
-
-        /// <summary>
-        /// Set the Character Hit Enum
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-        public void CharacterHitPicker_Changed(object sender, EventArgs args)
-        {
-            // Check for null, SelectedItem is not set when the control is created
-            if (CharacterHitPicker.SelectedItem == null)
-            {
-                return;
-            }
-
-            // Change the Difficulty
-            BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.CharacterHitEnum = HitStatusEnumHelper.ConvertStringToEnum(CharacterHitPicker.SelectedItem.ToString());
+            AllowAmazonDeliverySwitch.IsToggled = BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.AllowAmazonDelivery;
         }
 
         /// <summary>
@@ -124,55 +39,20 @@ namespace Game.Views
         }
 
         /// <summary>
-        /// Toggle Critical Miss
+        /// Toggle Amazon Delivery
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        public void AllowCriticalMiss_Toggled(object sender, EventArgs e)
+        public void AllowAmazonDelivery_Toggled(object sender, EventArgs e)
         {
             // Flip the settings
-            if (AllowCriticalMissSwitch.IsToggled == true)
+            if (AllowAmazonDeliverySwitch.IsToggled == true)
             {
-                BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.AllowCriticalMiss = true;
+                BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.AllowAmazonDelivery = true;
                 return;
             }
 
-            BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.AllowCriticalMiss = false;
+            BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.AllowAmazonDelivery = false;
         }
-
-        /// <summary>
-        /// Toggle Critical Hit
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void AllowCriticalHit_Toggled(object sender, EventArgs e)
-        {
-            // Flip the settings
-            if (AllowCriticalHitSwitch.IsToggled == true)
-            {
-                BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.AllowCriticalHit = true;
-                return;
-            }
-
-            BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.AllowCriticalHit = false;
-        }
-
-        /// <summary>
-        /// Toggle Monster Items
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void AllowMonsterItems_Toggled(object sender, EventArgs e)
-        {
-            // Flip the settings
-            if (AllowMonsterItemsSwitch.IsToggled == true)
-            {
-                BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.AllowMonsterItems = true;
-                return;
-            }
-
-            BattleEngineViewModel.Instance.Engine.EngineSettings.BattleSettingsModel.AllowMonsterItems = false;
-        }
-
     }
 }
