3e5495516185ecc3abe248d5c88ffa499f6300be
Michael Arvan <arvanmichael@seattleu.edu>
2021-03-15 22:48:40 -0700

    Added UT BattlePage_SaveScoreButton_Clicked_Default_Should_Pass() and BattlePage_ScoreNameEntry_TextChanged_Default_Should_Pass() to BattlePageTest.cs

--- UnitTests/Views/Battle/BattlePageTests.cs
+++ UnitTests/Views/Battle/BattlePageTests.cs
@@ -110,6 +110,34 @@ namespace UnitTests.Views
         }
 
         [Test]
+        public void BattlePage_SaveScoreButton_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.SaveScoreButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void BattlePage_ScoreNameEntry_TextChanged_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.ScoreNameEntry_TextChanged(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
         public void BattlePage_ShowScoreButton_Clicked_Default_Should_Pass()
         {
             // Arrange
