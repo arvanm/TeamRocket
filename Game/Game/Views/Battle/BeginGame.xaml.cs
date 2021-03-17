40e6d6f31e946bfd49f0a7190c73a02b5b7eb42b
Michael Arvan <arvanmichael@seattleu.edu>
2021-03-17 08:17:42 -0700

    Add Comments to StartGame button in BeginGame.xaml.cs

--- Game/Game/Views/Battle/BeginGame.xaml.cs
+++ Game/Game/Views/Battle/BeginGame.xaml.cs
@@ -21,7 +21,11 @@ namespace Game.Views
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Start Button move page to next battle page view, which is picking characters
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public async void StartGame_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new PickCharactersPage());
